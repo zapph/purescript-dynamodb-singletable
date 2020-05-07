10e6378db243d596977989b4eccf2700cfe37002
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-05-07 17:31:16 +0800

    Add note to support empty update

--- test/AWS/DynamoDB/SingleTable/UpdateExpressionSpec.purs
+++ test/AWS/DynamoDB/SingleTable/UpdateExpressionSpec.purs
@@ -5,11 +5,13 @@ import Prelude
 import AWS.DynamoDB.SingleTable.AttributeValue (avN, avS)
 import AWS.DynamoDB.SingleTable.UpdateExpression (mkUpdate)
 import Foreign.Object as Object
-import Test.Spec (Spec, describe, it)
+import Test.Spec (Spec, describe, it, pending)
 import Test.Spec.Assertions (shouldEqual)
 
 spec :: Spec Unit
 spec = describe "UpdateExpression" do
+  pending "empty update should not emit empty expression"
+
   it "should build set commands" do
     let u = mkUpdate { "A": 1.2, "B": "foo" }
     u.expression `shouldEqual` "SET #B = :B, #A = :A" -- this can end up jumbled
