6aefb8308519127fe378267e08cd1adf86c79b10
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-06-06 13:49:57 +0800

    Add buildParams for ConditionExpression

--- src/AWS/DynamoDB/SingleTable/ConditionExpression.purs
+++ src/AWS/DynamoDB/SingleTable/ConditionExpression.purs
@@ -13,17 +13,22 @@ module AWS.DynamoDB.SingleTable.ConditionExpression
        , class CanContain
        , class Containable
        , cContains
+       , buildParams
        ) where
 
 import Prelude
 
-import AWS.DynamoDB.SingleTable.AttributeValue (class AVCodec, AttributeValue, writeAV)
-import AWS.DynamoDB.SingleTable.Types (Path, spToPath)
+import AWS.DynamoDB.SingleTable.AttributeValue (class AVCodec, AttributeValue, avS, writeAV)
+import AWS.DynamoDB.SingleTable.CommandBuilder (CommandBuilder)
+import AWS.DynamoDB.SingleTable.CommandBuilder as CB
+import AWS.DynamoDB.SingleTable.Types (Path, pathToString, spToPath)
 import Data.Array.NonEmpty (NonEmptyArray)
-import Data.Exists (Exists, mkExists)
+import Data.Exists (Exists, mkExists, runExists)
+import Data.Foldable (intercalate)
 import Data.Maybe (Maybe)
 import Data.Set.NonEmpty (NonEmptySet)
 import Data.Symbol (class IsSymbol, SProxy)
+import Data.Traversable (traverse)
 import Prim.Row as Row
 
 -- https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html
@@ -51,7 +56,7 @@ type Operand' (r :: # Type) =
 
 data Comparator =
   CompEq
-  | CompNeq
+  | CompNEq
   | CompLt
   | CompLtEq
   | CompGt
@@ -165,3 +170,71 @@ opValue ::
   v ->
   Operand r v
 opValue = OValue <<< writeAV
+
+buildParams ::
+  forall r.
+  Condition r ->
+  CommandBuilder String
+buildParams (CComp lhs cmp rhs) = ado
+  lhs' <- buildOp' lhs
+  rhs' <- buildOp' rhs
+  in lhs' <> " " <> printComp cmp <> " " <> rhs'
+buildParams (CBetween a { min, max }) = ado
+  a' <- buildOp' a
+  min' <- buildOp' min
+  max' <- buildOp' max
+  in (a' <> " BETWEEN " <> min' <> " AND " <> max')
+buildParams (CIn a ops) = ado
+  a' <- buildOp' a
+  ops' <- traverse buildOp' ops
+  in a' <> " IN (" <> intercalate ", " ops' <> ")"
+buildParams (CAnd c1 c2) = ado
+  c1' <- buildParams c1
+  c2' <- buildParams c2
+  in wrap c1' <> " AND " <> wrap c2'
+buildParams (COr c1 c2) = ado
+  c1' <- buildParams c1
+  c2' <- buildParams c2
+  in wrap c1' <> " OR " <> wrap c2'
+buildParams (CNot c) = ado
+  c' <- buildParams c
+  in "NOT " <> wrap c'
+buildParams (CAttributeExists p) = ado
+  p' <- CB.addName (pathToString p)
+  in "attribute_exists(" <> p' <> ")"
+buildParams (CAttributeNotExists p) = ado
+  p' <- CB.addName (pathToString p)
+  in "attribute_note_exists(" <> p' <> ")"
+buildParams (CBeginsWith p substr) = ado
+  p' <- CB.addName (pathToString p)
+  substr' <- CB.addValue (avS substr)
+  in "begins_with(" <> p' <> ", " <> substr' <> ")"
+buildParams (CContains c a) = ado
+  c' <- CB.addName (pathToString c)
+  a' <- buildOp' a
+  in "contains(" <> c' <> ", " <> a' <> ")"
+
+buildOp' ::
+  forall r.
+  Operand' r ->
+  CommandBuilder String
+buildOp' op = runExists buildOp op
+
+buildOp ::
+  forall r v.
+  Operand r v ->
+  CommandBuilder String
+buildOp (OPath p) = CB.addName (pathToString p)
+buildOp (OValue v) = CB.addValue v
+
+-- utils
+wrap :: String -> String
+wrap b = "(" <> b <> ")"
+
+printComp :: Comparator -> String
+printComp CompEq = "="
+printComp CompNEq = "<>"
+printComp CompLt = "<"
+printComp CompLtEq = "<="
+printComp CompGt = ">"
+printComp CompGtEq = ">="
