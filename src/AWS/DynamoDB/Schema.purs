f08eca300ad30bd7081465a22fc7069220016a22
Jan Vincent Liwanag <jvliwanag@gmail.com>
2021-02-25 18:06:02 +0800

    Add support for reading ix

--- src/AWS/DynamoDB/Schema.purs
+++ src/AWS/DynamoDB/Schema.purs
@@ -9,23 +9,43 @@ module AWS.DynamoDB.SingleTable.Schema
        , IxCons1
        , IxHead1
        , IxConsLast
+       , IxList1Proxy(..)
        , type (:#)
        , type (:#:)
        , class IxWritePart
        , ixWritePart
+       , class IxReadPart
+       , ixReadPart
        , class MkIxValue
        , mkIxValue
+       , class ReadIxValue
+       , readIxValue'
+       , readIxValue
+       , readIxValue_
+       , readIxValueContent
        ) where
 
 import Prelude
 
-import AWS.DynamoDB.SingleTable.DynText (class PrintDynText, printDynText)
+import AWS.DynamoDB.SingleTable.DynText (class KeySegmentCodec, decodeKeySegment, encodeKeySegment)
 import AWS.DynamoDB.SingleTable.Utils.SymbolUtils (class IsWordAllUpper1)
+import Control.MonadPlus (guard)
+import Data.Array ((!!))
+import Data.Array as Array
+import Data.Maybe (Maybe(..))
+import Data.String as String
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Prim.Row as Row
 import Record as Record
+import Record.Builder as RecordBuilder
 
 newtype IxValue (l :: IxList1) = IxValue String
+
+derive instance ixValueEq :: Eq (IxValue l)
+derive instance ixValueOrd :: Ord (IxValue l)
+
+instance ixValueShow :: Show (IxValue l) where
+  show (IxValue s) = "(IxValue " <> s <> ")"
 
 printIxValue :: forall l. IxValue l -> String
 printIxValue (IxValue s) = s
@@ -36,6 +56,7 @@ foreign import data IxDyn :: Symbol -> Type -> IxPart
 
 data IxPartProxy (p :: IxPart) = IxPartProxy
 
+-- TODO rename Head1 -> Last1
 foreign import kind IxList1
 foreign import data IxCons1 :: IxPart -> IxList1 -> IxList1
 foreign import data IxHead1 :: IxPart -> IxList1
@@ -44,6 +65,8 @@ type IxConsLast h l = IxCons1 h (IxHead1 l)
 
 infixr 6 type IxCons1 as :#
 infixr 6 type IxConsLast as :#:
+
+data IxList1Proxy (l :: IxList1) = IxList1Proxy
 
 class IxWritePart (p :: IxPart) (r :: # Type) where
   ixWritePart :: IxPartProxy p -> {|r} -> String
@@ -58,13 +81,39 @@ instance ixWritePartConst ::
 instance ixWritePartDyn ::
   ( IsSymbol n
   , Row.Cons n t _r r
-  , PrintDynText t
+  , KeySegmentCodec t
   ) => IxWritePart (IxDyn n t) r where
 
   ixWritePart _ r =
-    "_" <> printDynText (Record.get (SProxy :: _ n) r)
+    "_" <> encodeKeySegment (Record.get (SProxy :: _ n) r)
 
-class MkIxValue (l :: IxList1) (r :: # Type) where
+class IxReadPart (p :: IxPart) (r1 :: # Type) (r2 :: # Type) | p -> r1 r2 where
+  ixReadPart :: IxPartProxy p -> String -> Maybe (RecordBuilder.Builder {|r1} {|r2})
+
+instance ixReadPartConst ::
+  ( IsWordAllUpper1 s
+  , IsSymbol s
+  ) => IxReadPart (IxConst s) r r where
+
+  ixReadPart _ s =
+    guard (s == reflectSymbol (SProxy :: _ s)) $> identity
+
+instance ixReadPartDyn ::
+  ( IsSymbol n
+  , Row.Cons n t r1 r2
+  , Row.Lacks n r1
+  , KeySegmentCodec t
+  ) => IxReadPart (IxDyn n t) r1 r2 where
+
+  ixReadPart _ s =
+    case String.splitAt 1 s of
+      { before: "_", after } ->
+        decodeKeySegment after <#> RecordBuilder.insert (SProxy :: _ n)
+      _ ->
+        Nothing
+
+
+class MkIxValue (l :: IxList1) (r :: # Type) | l -> r where
   mkIxValue :: {|r} -> IxValue l
 
 instance mkIxValueHead1 ::
@@ -84,3 +133,54 @@ instance mkIxValueCons1 ::
     $ ixWritePart (IxPartProxy :: _ p) r
     <> "#"
     <> printIxValue (mkIxValue r :: _ t)
+
+class ReadIxValue (l :: IxList1) (r1 :: # Type) (r2 :: # Type) | l -> r1 r2 where
+  readIxValue' :: IxList1Proxy l -> Array String -> Int -> Maybe (RecordBuilder.Builder {|r1} {|r2})
+
+instance readIxValueHead1 ::
+  IxReadPart p r1 r2 =>
+  ReadIxValue (IxHead1 p) r1 r2 where
+
+  readIxValue' _ as ndx = do
+    guard (Array.length as == ndx + 1)
+    (as !! ndx) >>= ixReadPart (IxPartProxy :: _ p)
+
+instance readIxValueCons1 ::
+  ( IxReadPart p r1 r2
+  , ReadIxValue t r2 r3
+  ) =>
+  ReadIxValue (IxCons1 p t) r1 r3 where
+
+  readIxValue' _ as ndx =
+    (>>>)
+    <$> ((as !! ndx) >>= ixReadPart (IxPartProxy :: _ p))
+    <*> readIxValue' (IxList1Proxy :: _ t) as (ndx + 1)
+
+readIxValue ::
+  forall l r.
+  ReadIxValue l () r =>
+  String ->
+  Maybe { ixv :: IxValue l, r :: {|r} }
+readIxValue s = b <#> \b' ->
+  { ixv: IxValue s
+  , r: RecordBuilder.build b' {}
+  }
+  where
+    b = readIxValue' (IxList1Proxy :: _ l) (String.split (String.Pattern "#") s) 0
+
+readIxValue_ ::
+  forall l r.
+  ReadIxValue l () r =>
+  String ->
+  Maybe (IxValue l)
+readIxValue_ s = _.ixv <$> readIxValue s
+
+readIxValueContent ::
+  forall l r.
+  ReadIxValue l () r =>
+  IxList1Proxy l ->
+  String ->
+  Maybe {|r}
+readIxValueContent p s = _.r <$> read
+  where
+    read = readIxValue s :: _ { ixv :: IxValue l, r :: {|r} }
--- test/AWS/DynamoDB/SingleTable/SchemaSpec.purs
+++ test/AWS/DynamoDB/SingleTable/SchemaSpec.purs
@@ -4,10 +4,12 @@ module AWS.DynamoDB.SingleTable.SchemaSpec
 
 import Prelude
 
-import AWS.DynamoDB.SingleTable.Schema (type (:#), type (:#:), IxConst, IxDyn, IxHead1, IxValue, mkIxValue, printIxValue)
+import AWS.DynamoDB.SingleTable.DynText (NormalizedText, normalizeText)
+import AWS.DynamoDB.SingleTable.Schema (class MkIxValue, class ReadIxValue, type (:#), type (:#:), IxConst, IxDyn, IxHead1, IxList1Proxy(..), IxValue, mkIxValue, printIxValue, readIxValue, readIxValue_)
+import Data.Maybe (isNothing)
 import Effect.Aff (Aff)
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (shouldContain, shouldEqual, shouldSatisfy)
 
 {-
 type Schema =
@@ -20,10 +22,11 @@ type Schema =
 
 schemaSpec :: Spec Unit
 schemaSpec = describe "schema" do
-  ixSpec
+  ixWriteSpec
+  ixReadSpec
 
-ixSpec :: Spec Unit
-ixSpec = describe "ix" do
+ixWriteSpec :: Spec Unit
+ixWriteSpec = describe "ix write" do
   it "should write single const ix" do
     (mkIxValue {} :: _ (IxHead1 (IxConst "FOO")))
       `shouldPrintAs` "FOO"
@@ -33,13 +36,45 @@ ixSpec = describe "ix" do
       `shouldPrintAs` "FOO#BAR"
 
   it "should write const#dyn#const ix" do
-    (mkIxValue { bar: "baz" } :: _ (IxConst "FOO" :# IxDyn "bar" String :#: IxConst "QUX"))
+    (mkIxValue { bar: normalizeText "baz" } :: _ (IxConst "FOO" :# IxDyn "bar" NormalizedText :#: IxConst "QUX"))
       `shouldPrintAs` "FOO#_baz#QUX"
+
+ixReadSpec :: Spec Unit
+ixReadSpec = describe "ix read" do
+  it "roundtrip" do
+    testRoundtrip (p :: _ (IxHead1 (IxConst "FOO"))) {}
+    testRoundtrip (p :: _ (IxConst "FOO" :#: IxConst "BAR")) {}
+    testRoundtrip (p :: _ (IxConst "FOO" :# IxDyn "bar" NormalizedText :#: IxConst "QUX")) { bar: normalizeText "baz" }
+
+  it "should fail on const mismatch" do
+    (readIxValue_ "BAR" :: _  (_ (IxHead1 (IxConst "FOO"))))
+      `shouldSatisfy` isNothing
+
+  it "should fail on wrong length" do
+    (readIxValue_ "FOO#BAR#BAZ" :: _ (_ (IxConst "FOO" :#: IxConst "BAR")))
+      `shouldSatisfy` isNothing
 
 shouldPrintAs :: forall l. IxValue l -> String -> Aff Unit
 shouldPrintAs ixValue s =
   printIxValue ixValue `shouldEqual` s
 
+testRoundtrip ::
+  forall l r.
+  MkIxValue l r =>
+  ReadIxValue l () r =>
+  Show {|r} =>
+  Eq {|r} =>
+  IxList1Proxy l ->
+  {|r} ->
+  Aff Unit
+testRoundtrip _ r =
+  readIxValue s `shouldContain` { ixv, r }
+  where
+    ixv = mkIxValue r :: _ l
+    s = printIxValue ixv
+
+p :: forall l. IxList1Proxy l
+p = IxList1Proxy
 -- The ff should not compile
 
 -- foo = mkIxValue {} :: _ (IxHead1 (IxConst "foo"))
