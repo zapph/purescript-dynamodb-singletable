92d4d6e5ba934d250b82b40cade3029594816be3
Jan Vincent Liwanag <jvliwanag@gmail.com>
2021-02-28 20:11:27 +0800

    Add split with util

--- src/AWS/DynamoDB/Utils/SymbolUtils.purs
+++ src/AWS/DynamoDB/Utils/SymbolUtils.purs
@@ -3,6 +3,9 @@ module AWS.DynamoDB.SingleTable.Utils.SymbolUtils
        , class IsWordAllUpper
        ) where
 
+import Data.Symbol (SProxy(..))
+import Prim.Boolean (kind Boolean)
+import Prim.Ordering (EQ, kind Ordering)
 import Prim.Symbol as Symbol
 
 class IsCharUpper (c :: Symbol)
@@ -42,3 +45,35 @@ else instance isWordAllUpperCons ::
   , IsCharUpper h
   , IsWordAllUpper t
   ) => IsWordAllUpper s
+
+foreign import kind SymbolList
+foreign import data SLNil :: SymbolList
+foreign import data SLCons :: Symbol -> SymbolList -> SymbolList
+
+infixr 4 type SLCons as :+
+
+class SymbolSplitWithChar (sep :: Symbol) (s :: Symbol) (r :: SymbolList) | sep s -> r
+
+instance symbolSplitWithCharI :: SymbolSplitWithChar' sep s "" r => SymbolSplitWithChar sep s r
+
+class SymbolSplitWithChar' (sep :: Symbol) (s :: Symbol) (acc :: Symbol) (r :: SymbolList) | sep s acc -> r
+
+instance symbolSplitWithCharNil :: SymbolSplitWithChar' sep "" acc (SLCons acc SLNil)
+else instance symbolSplitWithCharMatch ::
+  ( Symbol.Cons h tl s
+  , Symbol.Compare h sep cmp
+  , SymbolSplitAcc cmp sep h tl acc r
+  ) => SymbolSplitWithChar' sep s acc r
+
+class SymbolSplitAcc (cmp :: Ordering) (sep :: Symbol) (h :: Symbol) (tl :: Symbol) (acc :: Symbol) (r :: SymbolList) | sep cmp h tl acc -> r
+
+instance symbolSplitAccMatch ::
+  ( SymbolSplitWithChar sep tl tlR
+  ) => SymbolSplitAcc EQ sep h tl acc (SLCons acc tlR)
+
+else instance symbolSplitAccNoMatch ::
+  ( Symbol.Append acc h acc'
+  , SymbolSplitWithChar' sep tl acc' tlR
+  ) => SymbolSplitAcc ord sep h tl acc tlR
+
+data SLProxy (slist :: SymbolList) = SLProxy
