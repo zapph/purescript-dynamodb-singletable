bd055c81930b0493abc8331c0470f5b71c617e33
Jan Vincent Liwanag <jvliwanag@gmail.com>
2021-03-22 15:21:40 +0800

    Add test util for equal without show

---
+++ test/AWS/DynamoDB/SingleTable/TestUtils.js
@@ -0,0 +1,3 @@
+exports.showAny = function (a) {
+  return JSON.stringify(a);
+};
--- test/AWS/DynamoDB/SingleTable/TestUtils.purs
+++ test/AWS/DynamoDB/SingleTable/TestUtils.purs
@@ -1,7 +1,12 @@
 module AWS.DynamoDB.SingleTable.TestUtils where
 
+import Prelude
+
 import AWS.DynamoDB.SingleTable.Repo (Repo, mkRepo)
+import Control.Monad.Error.Class (class MonadThrow)
+import Effect.Exception (Error)
 import Literals.Undefined (undefined)
+import Test.Spec.Assertions (fail)
 import Unsafe.Coerce (unsafeCoerce)
 
 mkDummyRepo ::
@@ -12,3 +17,16 @@ mkDummyRepo { tableName } =
   mkRepo { tableName: tableName
          , dynamodb: unsafeCoerce undefined
          }
+
+shouldEqual'
+  :: forall m t
+   . MonadThrow Error m
+  => Eq t
+  => t
+  -> t
+  -> m Unit
+shouldEqual' v1 v2 =
+  when (v1 /= v2) $
+    fail $ showAny v1 <> " ≠ " <> showAny v2
+
+foreign import showAny :: forall a. a -> String
